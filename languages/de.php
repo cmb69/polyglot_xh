8037e9a86ea398300e8d96d428de85527031ce23
cmb69 <cmbecker69@gmx.de>
2013-05-01 22:40:56 +0000

    updated German localization

--- languages/de.php
+++ languages/de.php
@@ -4,13 +4,13 @@ $plugin_tx['polyglott']['menu_main']="Übersetzungen";
 
 $plugin_tx['polyglott']['not_translated']="Bedaure &ndash; diese Seite wurde noch nicht übersetzt.";
 
-// TODO: translate
+$plugin_tx['polyglott']['syscheck_title']="System-Prüfung";
+$plugin_tx['polyglott']['syscheck_phpversion']="PHP-Version &ge; %s";
+$plugin_tx['polyglott']['syscheck_extension']="Erweiterung '%s' geladen";
+$plugin_tx['polyglott']['syscheck_encoding']="UTF-8 Kodierung eingestellt";
+$plugin_tx['polyglott']['syscheck_magic_quotes']="magic_quotes_runtime off";
+$plugin_tx['polyglott']['syscheck_writable']="Ordner '%s' beschreibbar";
 
-$plugin_tx['polyglott']['syscheck_title']="System check";
-$plugin_tx['polyglott']['syscheck_phpversion']="PHP version &ge; %s";
-$plugin_tx['polyglott']['syscheck_extension']="Extension '%s' loaded";
-$plugin_tx['polyglott']['syscheck_encoding']="Encoding 'UTF-8' configured";
-$plugin_tx['polyglott']['syscheck_magic_quotes']="Magic quotes runtime off";
-$plugin_tx['polyglott']['syscheck_writable']="Folder '%s' writable";
+$plugin_tx['polyglott']['about']="Über Polyglott_XH";
 
 ?>
--- languages/en.php
+++ languages/en.php
@@ -8,7 +8,7 @@ $plugin_tx['polyglott']['syscheck_title']="System check";
 $plugin_tx['polyglott']['syscheck_phpversion']="PHP version &ge; %s";
 $plugin_tx['polyglott']['syscheck_extension']="Extension '%s' loaded";
 $plugin_tx['polyglott']['syscheck_encoding']="Encoding 'UTF-8' configured";
-$plugin_tx['polyglott']['syscheck_magic_quotes']="Magic quotes runtime off";
+$plugin_tx['polyglott']['syscheck_magic_quotes']="magic_quotes_runtime off";
 $plugin_tx['polyglott']['syscheck_writable']="Folder '%s' writable";
 
 $plugin_tx['polyglott']['about']="About Polyglott_XH";

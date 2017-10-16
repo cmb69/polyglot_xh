b01f95f3943ae9f091004420585da44bed2553a8
Christoph M. Becker <cmbecker69@gmx.de>
2017-10-16 17:34:26 +0200

    Use classes instead of inline styles

--- views/info.php
+++ views/info.php
@@ -1,17 +1,17 @@
 <h1>Polyglott</h1>
 <img src="<?=$this->icon()?>" alt="<?=$this->text('alt_logo')?>"
-     style="float: left; width: 128px; height: 128px; margin-right: 16px"/>
+     class="polyglott_logo"/>
 <p>Version: <?=$this->version()?></p>
 <p>Copyright © 2012-2017 <a href="http://3-magi.net/">Christoph M. Becker</a></p>
-<p style="text-align: justify">This program is free software: you can
+<p class="polyglott_license">This program is free software: you can
 redistribute it and/or modify it under the terms of the GNU General Public
 License as published by the Free Software Foundation, either version 3 of the
 License, or (at your option) any later version.</p>
-<p style="text-align: justify">This program is distributed in the hope that it
+<p class="polyglott_license">This program is distributed in the hope that it
 will be useful, but <em>without any warranty</em>; without even the implied
 warranty of <em>merchantability</em> or <em>fitness for a particular
 purpose</em>. See the GNU General Public License for more details.</p>
-<p style="text-align: justify">You should have received a copy of the GNU
+<p class="polyglott_license">You should have received a copy of the GNU
 General Public License along with this program. If not, see
 <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.</p>
 <h4><?=$this->text('syscheck_title')?></h4>
